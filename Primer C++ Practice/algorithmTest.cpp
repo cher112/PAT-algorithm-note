4a86a32b2d3260c6421692567d75cea9643e559e
cher112 <961162300@qq.com>
2020-10-13 22:02:24 +0800

    增加了算法标准库的测试

---
+++ Primer C++ Practice/algorithmTest.cpp
@@ -0,0 +1,22 @@
+# include <stdio.h>
+# include <iostream>
+# include <algorithm>
+# include <vector>
+# include <typeinfo>
+using namespace std;
+
+int main(){
+    vector<int> a{6,3,5,4,2};
+    int b[] = {4,6,3,7,5};
+    //reverse
+    reverse(a.begin(),a.end());
+    for(auto i:a){ cout<<i<<" ";}// this i means the key of vector[ key ]
+    for(auto i:b){cout<<i<<" ";}
+    //sort()
+    cout<<endl;
+    sort(a.begin(),a.end());
+    sort(b,b+5);
+    for(auto i:a){ cout<<i<<" ";}
+    for(auto i:b){ cout<<i<<" ";}
+
+}
